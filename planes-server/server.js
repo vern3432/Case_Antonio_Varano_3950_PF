3a25b4778f227f4761733a7296dfa3e8417f2a6c
Aidan_V_WIN <varanoab@merrimack.edu>
2023-12-04 13:39:23 -0500

    Basics for server and unrefactored scipt to create database. Will databse in meeting

--- planes-server/server.js
+++ planes-server/server.js
@@ -1,1 +1,10 @@
 //TODO For the back end
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+const app = express();
+const port = 3001;
+// var async=require('async')
+
+// const dbPath = path.resolve(__dirname, "memory.db");
+// const db = new sqlite3.Database(dbPath);
