f359761af4a1c72c1b8d86f5d815d615b0e7136b
Mark Case <casem@merrimack.edu>
2023-12-05 19:33:43 -0500

    db creation

--- planes-server/script.js
+++ planes-server/script.js
@@ -1,50 +1,49 @@
 const sql3 = require("better-sqlite3");
-console.log("imported");
 
 const db = new sql3("memory.db");
-// const csv = require("csv-parser");
-// const fs = require("fs");
-console.log("imported");
-// create table
 
-//  db.exec( 'DROP TABLE menuItems;' );
-db.exec("DROP TABLE IF EXISTS Users");
-db.exec("DROP TABLE IF EXISTS Reservations");
-db.exec("DROP TABLE IF EXISTS Planes");
-console.log("table dropped");
-db.exec(
-  "CREATE TABLE IF NOT EXISTS Users (email TEXT PRIMARY KEY, user_type TEXT);"
-);
-console.log("table Users");
-db.exec(
-  "CREATE TABLE IF NOT EXISTS Planes (planid TEXT PRIMARY KEY, make TEXT, model TEXT,imagelink TEXT);"
-);
-console.log("table Planes");
-//FOREIGN KEY(client1) REFERENCES Users(email)),FOREIGN KEY(instructor) REFERENCES Users(email)),FOREIGN KEY(client1) REFERENCES Users(email)),FOREIGN KEY(client2) REFERENCES Users(email)),FOREIGN KEY(planid) REFERENCES Planes(planid));
-db.exec(
-  "CREATE TABLE IF NOT EXISTS Reservations (DateTime TEXT, PlaneID TEXT,client1 TEXT,FOREIGN KEY(client1) REFERENCES Users(email), client2 TEXT,FOREIGN KEY(client2) REFERENCES Users(email),instructor TEXT,FOREIGN KEY(instructor) REFERENCES Users(email),planid TEXT,FOREIGN KEY(planid) REFERENCES Planes(planid));"
-);
+const createTablesQuery = `
+    -- Create User Table
+    CREATE TABLE IF NOT EXISTS user (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email VARCHAR NOT NULL,
+        user_type VARCHAR NOT NULL
+    );
 
-// db.exec(
-//   "CREATE TABLE IF NOT EXISTS Users (email TEXT PRIMARY KEY, user TEXT);"
-// );
+    -- Create Plane Table
+    CREATE TABLE IF NOT EXISTS plane (
+        plane_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Year YEAR,
+        make VARCHAR NOT NULL,
+        model VARCHAR NOT NULL,
+        img_src VARCHAR NOT NULL,
+        tail_number VARCHAR NOT NULL 
+    );
 
-db.exec("DROP TABLE IF EXISTS Users");
-console.log("table dropped");
-db.exec("CREATE TABLE IF NOT EXISTS Users (email TEXT, user TEXT);");
-console.log("executed");
+    -- Create Comments Table
+    CREATE TABLE IF NOT EXISTS comments (
+        comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        comment VARCHAR
+    );
 
-// const insrow = db.prepare(
-//   "insert into Users (ISBN,Book_Title,Book_Author) VALUES (?, ?)"
-// );
-// fs.createReadStream('data2.csv')
-//   .pipe(csv({"separator":","}))
-//   .on('data', (row) => {
-//     console.log(row)
-//     insrow.run(row.ISBN,row.Book_Title,row.Book_Author,row.Year_Of_Publication,row.Publisher,row.Image_URL_S,row.Image_URL_M,row.Image_URL_L,'');
-//     console.log(row);
-//   })
-//   .on('end', () => {
-//     console.log('CSV file successfully processed');
-//     db.close();
-//   });
+    -- Create Reservations Table
+    CREATE TABLE IF NOT EXISTS reservations (
+        reservation_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        datetime DATETIME NOT NULL,
+        flighttype VARCHAR NOT NULL,
+        user_id_1 INTEGER NOT NULL,
+        user_id_2 INTEGER,
+        plane_id INTEGER NOT NULL,
+        comment_id INTEGER,
+        FOREIGN KEY (user_id_1) REFERENCES user(user_id),
+        FOREIGN KEY (user_id_2) REFERENCES user(user_id),
+        FOREIGN KEY (plane_id) REFERENCES plane(plane_id),
+        FOREIGN KEY (comment_id) REFERENCES comments(comment_id)
+    );
+`;
+
+db.exec(createTablesQuery);
+
+const viewPlaneData = db.prepare("SELECT * FROM plane");
+console.log("\nPlane Table:");
+console.log(viewPlaneData.all());
