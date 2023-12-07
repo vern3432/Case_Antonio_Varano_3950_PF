0a015d864108e17f23e4deda51c521835fba653a
Aidan_V_WIN <varanoab@merrimack.edu>
2023-12-06 22:58:51 -0500

    Server function for email

--- planes-server/server.js
+++ planes-server/server.js
@@ -1,9 +1,11 @@
+//npm install nodemailer for emailing system 
+
 const express = require("express");
 const cors = require('cors');
 const sql3 = require("better-sqlite3");
 const db = new sql3("memory.db");
 const path = require("path");
-
+var nodemailer = require('nodemailer');
 
 const app = express();
 const port = 3001;
@@ -13,6 +15,51 @@ app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
+});
+const bodyParser = require("body-parser");
+//to check functionality, login in like this. There is no auth or anything on the account. its jsut gmail
+//planeservice7@gmail.com
+//123Planes!
+app.use(bodyParser.json());
+app.post("/send-email", async (req, res) => {
+    const subject = req.body.subject;
+    const userEmail = req.body.email;
+    const content = req.body.content;
+    const ownerEmail = "planeservice7@gmail.com"; 
+    const password = "123Planes!";
+  try {
+    const { userName, userEmail, message } = req.body;
+
+    // Replace these with your actual email configuration
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: ownerEmail,
+        pass: password,
+      },
+    });
+    const mailOptions = {
+      from: ownerEmail, 
+      to: ownerEmail,
+      cc: userEmail,
+      subject: Subject,
+      text: `Name: ${userName}\nEmail: ${userEmail}\nMessage: ${message}`,
+    };
+
+    // Send email
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ success: true, message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
 // Handle google oauth and login form
@@ -62,3 +109,5 @@ app.post("/newUser", (req, res) => {
         res.json({ success: true });
     }
 });
+
+
--- planes-ui/src/components/contact/Contact.jsx
+++ planes-ui/src/components/contact/Contact.jsx
@@ -1,5 +1,40 @@
-function Contact() {
-    return <div>Contact Page</div>;
+
+function validateForm() {
+  console.log("actually running");
+  const regemail =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  const basic_regex = new RegExp(regemail);
+  const isValidEmail = basic_regex.test(
+    document.getElementById("txtEmail").value
+  );
+
+  if (isValidEmail == false) {
+    document.getElementById("lblError").innerHTML = "Enter valid email Address";
+    return false;
+  } else {
+    document.getElementById("lblError").innerHTML = "";
+    return true;
   }
+}
+
+
+function Contact() {
+return (
+    <div>
+      <h1>Content Page</h1>
+      <form onSubmit={handleSubmit}>
+        {/* Input field with onChange handler to update state */}
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter something"
+        />
+        {/* Submit button */}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
