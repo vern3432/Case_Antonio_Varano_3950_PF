7f2438488610de32c3083928b0a755d508f73142
Aidan <varanoab@merrimack.eduvaranoab@merrimack.edu>
2023-12-06 23:39:02 -0500

    updated form for contact

--- planes-ui/src/components/contact/Contact.jsx
+++ planes-ui/src/components/contact/Contact.jsx
@@ -17,24 +17,77 @@ function validateForm() {
   }
 }
 
+import React, { useState } from 'react';
 
-function Contact() {
-return (
+const EmailForm = () => {
+  const [formData, setFormData] = useState({
+    userName: '',
+    userEmail: '',
+    message: '',
+    subject: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3001/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await response.json();
+
+      if (result.success) {
+        console.log('Email sent successfully');
+        // Handle success (e.g., show a success message to the user)
+      } else {
+        console.error('Failed to send email');
+        // Handle failure (e.g., show an error message to the user)
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
     <div>
-      <h1>Content Page</h1>
+      <h2>Email Form</h2>
       <form onSubmit={handleSubmit}>
-        {/* Input field with onChange handler to update state */}
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          placeholder="Enter something"
-        />
-        {/* Submit button */}
-        <button type="submit">Submit</button>
+        <label>
+          Name:
+          <input type="text" name="userName" value={formData.userName} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="userEmail" value={formData.userEmail} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Subject:
+          <input type="text" name="subject" value={formData.subject} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Message:
+          <textarea name="message" value={formData.message} onChange={handleChange} />
+        </label>
+        <br />
+        <button type="submit">Send Email</button>
       </form>
     </div>
   );
 };
 
-export default Contact;
+export default EmailForm;
