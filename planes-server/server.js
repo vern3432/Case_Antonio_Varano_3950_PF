8f33c495a34ad35981efc324af618b81ba30db3c
marky_boi <123032217+markyboi10@users.noreply.github.com>
2023-12-13 21:52:01 -0500

    disable sign up

--- planes-server/server.js
+++ planes-server/server.js
@@ -80,23 +80,23 @@ app.get("/get-employee", async (req, res) => {
 // WHERE user_id_1 = ? OR user_id_2 = ?;
 app.post('/fetch-user-reserver', (req, res) => {
   const { id } = req.body;
-   console.log(id)
-   let result;
-   let checkEmailQuery;
-   if (id !== undefined) {
-     checkEmailQuery = db.prepare("SELECT * FROM reservations WHERE user_id_1 = ?");
-     result = checkEmailQuery.get(id);
-   } else {
- 
-   }
- 
-   if (result) {
-     console.log(result);
-     res.send(result);
-   } else {
-     res.status(500).send("error");
-   }
- });
+  console.log(id)
+  let result;
+  let checkEmailQuery;
+  if (id !== undefined) {
+    checkEmailQuery = db.prepare("SELECT * FROM reservations WHERE user_id_1 = ?");
+    result = checkEmailQuery.get(id);
+  } else {
+
+  }
+
+  if (result) {
+    console.log(result);
+    res.send(result);
+  } else {
+    res.status(500).send("error");
+  }
+});
 
 app.post("/get-employee2", async (req, res) => {
   const ID = req.body.ID;
@@ -119,28 +119,28 @@ app.post("/get-employee2", async (req, res) => {
   }
 });
 
-// app.post("/get-user", async (req, res) => {
-//   const email = req.body.email;
-//   console.log(email);
-//   console.log("getting user");
-//   let result;
-//   let checkEmailQuery;
-//   if (email !== undefined) {
-//     checkEmailQuery = db.prepare("SELECT * FROM user WHERE email = ?");
-//     result = checkEmailQuery.get(email);
-//     console.log(result)
-//   } else {
-//     checkEmailQuery = db.prepare("SELECT * FROM user WHERE email = ?");
-//     result = checkEmailQuery.get("mark.case102@gmail.com");
-//     console.log(result)
-//   }
-//   if (result) {
-//     console.log(result);
-//     res.send(result);
-//   } else {
-//     res.status(500).send("error");
-//   }
-// });
+app.post("/get-user", async (req, res) => {
+  const email = req.body.email;
+  console.log(email);
+  console.log("getting user");
+  let result;
+  let checkEmailQuery;
+  if (email !== undefined) {
+    checkEmailQuery = db.prepare("SELECT * FROM user WHERE email = ?");
+    result = checkEmailQuery.get(email);
+    console.log(result)
+  } else {
+    checkEmailQuery = db.prepare("SELECT * FROM user WHERE email = ?");
+    result = checkEmailQuery.get("mark.case102@gmail.com");
+    console.log(result)
+  }
+  if (result) {
+    console.log(result);
+    res.send(result);
+  } else {
+    res.status(500).send("error");
+  }
+});
 
 
 app.get("/get-users", async (req, res) => {
